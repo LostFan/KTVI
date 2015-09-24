d5fb0215d33524b5b9cdc197d07f244242ff6d09
Ihar_Niakhlebau <Ihar_Niakhlebau@epam.com>
2015-09-24 13:20:09 +0300

    Implement all PaymentDAO methods. Implement all tests fot them.

--- src/main/java/org/lostfan/ktv/dao/PaymentDAO.java
+++ src/main/java/org/lostfan/ktv/dao/PaymentDAO.java
@@ -4,19 +4,32 @@ import java.time.LocalDate;
 import java.util.List;
 
 import org.lostfan.ktv.domain.Payment;
+import org.lostfan.ktv.domain.PaymentType;
 import org.lostfan.ktv.domain.Subscriber;
 
 public interface PaymentDAO {
 
     List<Payment> getAllPayments();
 
+    Payment getPayment(int id);
+
     List<Payment> getPaymentsByDate(LocalDate date);
 
-    List<Payment> getPaymentsBySubscriber(int subscriberId);
+    List<Payment> getPaymentsBySubscriberId(int subscriberId);
 
-    void save(Payment tariff);
+    void save(Payment payment);
 
-    void update(Payment tariff);
+    void update(Payment payment);
 
     void delete(int id);
+
+    List<PaymentType> getAllPaymentTypes();
+
+    PaymentType getPaymentType(int id);
+
+    void savePaymentType(PaymentType paymentType);
+
+    void updatePaymentType(PaymentType paymentType);
+
+    void deletePaymentType(int paymentTypeId);
 }
