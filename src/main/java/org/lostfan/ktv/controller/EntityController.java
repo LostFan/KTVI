38818414fc2f27322f5cad9979bfa278880a984d
Romman <romman.dev@gmail.com>
2015-12-04 17:11:25 +0300

    Make EntityController extendable and RenderedServiceController is a derived class.

--- src/main/java/org/lostfan/ktv/controller/EntityController.java
+++ src/main/java/org/lostfan/ktv/controller/EntityController.java
@@ -3,106 +3,75 @@ package org.lostfan.ktv.controller;
 import org.lostfan.ktv.domain.Entity;
 import org.lostfan.ktv.model.FieldSearchCriterion;
 import org.lostfan.ktv.model.entity.EntityModel;
-import org.lostfan.ktv.utils.ViewActionListener;
 import org.lostfan.ktv.validation.ValidationResult;
-import org.lostfan.ktv.view.EntityInnerTableView;
 import org.lostfan.ktv.view.EntitySearchView;
 import org.lostfan.ktv.view.EntityTableView;
 import org.lostfan.ktv.view.EntityView;
 import org.lostfan.ktv.view.components.EntityViewFactory;
 
-import java.util.ArrayList;
 import java.util.List;
-import java.util.Map;
 
 public class EntityController {
 
     private EntityModel model;
+    private EntitySearchView entitySearchView;
 
     public EntityController(EntityModel model, EntityTableView view) {
         this.model = model;
 
-        view.setFindActionListener(new FindActionListener());
-        view.setAddActionListener(new AddActionListener());
-        view.setChangeActionListener(new ChangeActionListener());
-        view.setDeleteActionListener(new DeleteActionListener());
+        view.setFindActionListener(this::findActionPerformed);
+        view.setAddActionListener(this::addActionPerformed);
+        view.setChangeActionListener(this::changeActionPerformed);
+        view.setDeleteActionListener(this::deleteActionPerformed);
     }
 
     public void setModel(EntityModel model) {
         this.model = model;
     }
 
-    private class FindActionListener implements ViewActionListener {
-        @Override
-        public void actionPerformed(Object args) {
-            EntitySearchView entitySearchView = new EntitySearchView(model);
-            entitySearchView.setFindActionListener(new SearchFindActionListener(entitySearchView));
-        }
+    protected void findActionPerformed(Object args) {
+        this.entitySearchView = new EntitySearchView(model);
+        this.entitySearchView.setFindActionListener(this::searchFindActionPerformed);
     }
 
-    private class SearchFindActionListener implements ViewActionListener {
-
-        EntitySearchView entitySearchView;
-
-        public SearchFindActionListener(EntitySearchView entitySearchView) {
-            this.entitySearchView = entitySearchView;
-            List<FieldSearchCriterion> criteria = this.entitySearchView.getSearchCriteria();
-            model.setSearchCriteria(criteria);
-        }
-
-        @Override
-        public void actionPerformed(Object args) {
-//            List<FieldSearchCriterion> criteria = this.entitySearchView.getSearchCriteria();
-//            model.setSearchCriteria(criteria);
-        }
+    protected void searchFindActionPerformed(Object args) {
+        List<FieldSearchCriterion> criteria = this.entitySearchView.getSearchCriteria();
+        this.model.setSearchCriteria(criteria);
     }
 
-    private class AddActionListener implements ViewActionListener {
-
-        @Override
-        public void actionPerformed(Object args) {
-            EntityView entityView = EntityViewFactory.createForm(model);
-            entityView.setAddActionListener(args_ -> {
-                Entity entity = (Entity) args_;
-                ValidationResult result = model.save(entity);
-                if (result.hasErrors()) {
-                    entityView.showErrors(result.getErrors());
-                    return;
-                }
-                model.save(entity);
-                entityView.hide();
-            });
-        }
+    protected void addActionPerformed(Object args) {
+        EntityView entityView = EntityViewFactory.createForm(model);
+        entityView.setAddActionListener(args_ -> {
+            Entity entity = (Entity) args_;
+            ValidationResult result = model.save(entity);
+            if (result.hasErrors()) {
+                entityView.showErrors(result.getErrors());
+                return;
+            }
+            model.save(entity);
+            entityView.hide();
+        });
     }
 
-    private class ChangeActionListener implements ViewActionListener {
+    protected void changeActionPerformed(Object args) {
+        int selectedId = (Integer) args;
 
-        @Override
-        public void actionPerformed(Object args) {
-            int selectedId = (Integer) args;
+        Entity entity = model.getEntity(selectedId);
+        EntityView entityView = EntityViewFactory.createForm(model, entity);
 
-            Entity entity = model.getEntity(selectedId);
-            EntityView entityView = EntityViewFactory.createForm(model, entity);
-
-            entityView.setAddActionListener(args_ -> {
-                Entity entity1 = (Entity) args_;
-                ValidationResult result = model.save(entity1);
-                if (result.hasErrors()) {
-                    entityView.showErrors(result.getErrors());
-                    return;
-                }
-                entityView.hide();
-            });
-        }
+        entityView.setAddActionListener(args_ -> {
+            Entity entity1 = (Entity) args_;
+            ValidationResult result = model.save(entity1);
+            if (result.hasErrors()) {
+                entityView.showErrors(result.getErrors());
+                return;
+            }
+            entityView.hide();
+        });
     }
 
-    private class DeleteActionListener implements ViewActionListener {
-
-        @Override
-        public void actionPerformed(Object args) {
-            List<Integer> selectedIndexes = (List<Integer>) args;
-            model.deleteEntityByRow(selectedIndexes);
-        }
+    protected void deleteActionPerformed(Object args) {
+        List<Integer> selectedIndexes = (List<Integer>) args;
+        model.deleteEntityByRow(selectedIndexes);
     }
-
 }
--- src/main/java/org/lostfan/ktv/controller/RenderedServiceController.java
+++ src/main/java/org/lostfan/ktv/controller/RenderedServiceController.java
@@ -1,99 +1,25 @@
 package org.lostfan.ktv.controller;
 
-import java.util.List;
-
 import org.lostfan.ktv.domain.RenderedService;
-import org.lostfan.ktv.domain.RenderedService;
-import org.lostfan.ktv.model.FieldSearchCriterion;
-import org.lostfan.ktv.model.entity.EntityModel;
 import org.lostfan.ktv.model.entity.RenderedServiceEntityModel;
-import org.lostfan.ktv.utils.ViewActionListener;
-import org.lostfan.ktv.validation.ValidationResult;
-import org.lostfan.ktv.view.EntitySearchView;
 import org.lostfan.ktv.view.EntityTableView;
 import org.lostfan.ktv.view.EntityView;
 import org.lostfan.ktv.view.components.EntityViewFactory;
 
-public class RenderedServiceController {
+public class RenderedServiceController extends EntityController {
 
     private RenderedServiceEntityModel model;
 
     public RenderedServiceController(RenderedServiceEntityModel model, EntityTableView view) {
-        this.model = model;
-
-        view.setFindActionListener(new FindActionListener());
-        view.setAddActionListener(new AddActionListener());
-        view.setChangeActionListener(new ChangeActionListener());
-        view.setDeleteActionListener(new DeleteActionListener());
-    }
-
-    public void setModel(RenderedServiceEntityModel model) {
+        super(model, view);
         this.model = model;
     }
 
-    private class FindActionListener implements ViewActionListener {
-        @Override
-        public void actionPerformed(Object args) {
-            EntitySearchView entitySearchView = new EntitySearchView(model);
-            entitySearchView.setFindActionListener(new SearchFindActionListener(entitySearchView));
-        }
+    @Override
+    protected void changeActionPerformed(Object args) {
+        int selectedId = (Integer) args;
+
+        RenderedService entity = model.getEntity(selectedId);
+        EntityView entityView = EntityViewFactory.createRenderedServiceForm(model, entity);
     }
-
-    private class SearchFindActionListener implements ViewActionListener {
-
-        EntitySearchView entitySearchView;
-
-        public SearchFindActionListener(EntitySearchView entitySearchView) {
-            this.entitySearchView = entitySearchView;
-            List<FieldSearchCriterion> criteria = this.entitySearchView.getSearchCriteria();
-            ((EntityModel) model).setSearchCriteria(criteria);
-        }
-
-        @Override
-        public void actionPerformed(Object args) {
-//            List<FieldSearchCriterion> criteria = this.entitySearchView.getSearchCriteria();
-//            model.setSearchCriteria(criteria);
-        }
-    }
-
-    private class AddActionListener implements ViewActionListener {
-
-        @Override
-        public void actionPerformed(Object args) {
-            EntityView entityView = EntityViewFactory.createForm(model);
-            entityView.setAddActionListener(args_ -> {
-                RenderedService entity = (RenderedService) args_;
-                ValidationResult result = model.save(entity);
-                if (result.hasErrors()) {
-                    entityView.showErrors(result.getErrors());
-                    return;
-                }
-                model.save(entity);
-                entityView.hide();
-            });
-        }
-    }
-
-    private class ChangeActionListener implements ViewActionListener {
-
-        @Override
-        public void actionPerformed(Object args) {
-            int selectedId = (Integer) args;
-
-            RenderedService entity = model.getEntity(selectedId);
-            EntityView entityView = EntityViewFactory.createRenderedServiceForm(model, entity);
-
-
-        }
-    }
-
-    private class DeleteActionListener implements ViewActionListener {
-
-        @Override
-        public void actionPerformed(Object args) {
-            List<Integer> selectedIndexes = (List<Integer>) args;
-            model.deleteEntityByRow(selectedIndexes);
-        }
-    }
-
 }
